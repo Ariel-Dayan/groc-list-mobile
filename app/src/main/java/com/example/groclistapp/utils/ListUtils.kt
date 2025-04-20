67c2713d6b122babe11804975f26047c2b1ccc0e
Ariel <ariel0280@gmail.com>
2025-04-21 01:01:28 +0300

    add list util

--- app/src/main/java/com/example/groclistapp/utils/ListUtils.kt
+++ app/src/main/java/com/example/groclistapp/utils/ListUtils.kt
@@ -1,4 +1,18 @@
 package com.example.groclistapp.utils
 
-class listUtils {
+import android.widget.TextView
+import com.example.groclistapp.data.model.ShoppingListSummary
+
+class ListUtils private constructor() {
+    fun toggleNoCardListsMessage(textView: TextView, shoppingList: List<ShoppingListSummary>?) {
+        if (shoppingList.isNullOrEmpty()) {
+            textView.visibility = TextView.VISIBLE
+        } else {
+            textView.visibility = TextView.GONE
+        }
+    }
+
+    companion object {
+        val instance = ListUtils()
+    }
 }
